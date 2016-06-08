7e70eb354c0969261a4149ec591e45ddaa20279d
teners <dmemory7@ya.ru>
2016-06-08 18:00:54 +0300

    Fixed popup message

---
+++ scripts/scripts.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Sergey Sokolov on 6/8/16.
+ */
+var hide;
+
+hide = function(container) {
+    container.style.visibility = "hidden";
+    // document.getElementById("iwannajob").style.visibility = "hidden";
+};
