8299f0b8a83bfd36ea5320f8006ed8e8c638660b
teners <dmemory7@ya.ru>
2016-06-11 01:45:19 +0300

    Add loading animation

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -4,6 +4,15 @@
 var hide;
 
 hide = function(container) {
-    container.style.visibility = "hidden";
-    // document.getElementById("iwannajob").style.visibility = "hidden";
+    $(container).hide(500, function() {
+        container.style.visibility = "hidden";
+    });
 };
+
+
+$(document).ready(function(){
+    $("#header").fadeOut(0).fadeIn(700);
+    $("#photo").fadeOut(0).delay(700).show(1000);
+    $("#main").fadeOut(0).delay(1700).slideDown(700);
+    $("#footer").hide(0).delay(2400).show(700);
+});
