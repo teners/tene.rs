572e363b044820afabfded2b655695d767299c80
Sergei Sokolov <sergei.sokolov@semrush.com>
2025-09-29 21:45:53 +0200

    update to v3

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,7 @@
 import { defineConfig } from 'astro/config';
 
-import tailwind from "@astrojs/tailwind";
 import purgecss from 'astro-purgecss';
 
 export default defineConfig({
-  integrations: [tailwind(), purgecss()],
+  integrations: [purgecss()],
 });
